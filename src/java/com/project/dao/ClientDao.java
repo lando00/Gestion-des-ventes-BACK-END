fe53cda85321a45809268ceec6ccde68f12eb55a
lando00 <orlandojulien2@gmail.com>
2023-05-22 11:37:38 +0200

    Premier commit

---
+++ src/java/com/project/dao/ClientDao.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.project.dao;
+
+import com.project.entity.Client;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Orlando
+ */
+public class ClientDao {
+    public ClientDao(){}
+    public List<Client> getAllClient() {
+        List<Client> clientList = new ArrayList();
+        
+        return clientList;
+    }
+}
